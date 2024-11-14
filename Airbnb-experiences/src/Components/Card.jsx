242ae40dd308fc94da2f8700079d62d8e6375e3e
Shawn George <shawngeorge530@gmail.com>
2024-11-14 21:08:45 +1300

    modifying the card tiles to ensure accessibility

--- Airbnb-experiences/src/Components/Card.jsx
+++ Airbnb-experiences/src/Components/Card.jsx
@@ -5,15 +5,20 @@ import star from "../assets/star.png";
 export default function Card() {
     return (
         <div className="card">
-            <img src={katie} alt="katie" className="card-image"/>
+            <img 
+                src={katie} 
+                className="card-image"
+                alt="Main card image"
+
+            />
                 <div className="card-details" >
                     <img src={star} alt="star" className="card-star" />
-                    <span>5.0 </span>
-                    <span>(6) . </span>
+                    <span className="gray">5.0 </span>
+                    <span className="gray">(6) . </span>
                     <span> USA</span>
                 </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p> From $136 / person</p>
+            <h2>Life lessons with Katie Zaferes</h2>
+            <p> <span className="bold">From $136</span> / person</p>
         </div>
     )
 }
